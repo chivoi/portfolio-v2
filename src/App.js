62ba4d383a5031f19a084f7df56bdcb2de638b22
chivoi <a.lastovirya@gmail.com>
2021-06-20 16:25:15 +1000

    creates about, contact, projects, resume components, includes them into routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// styles
+import './App.scss';
 //components
 import Nav from './components/Nav';
 import Home from './components/Home';
-
-import './App.scss';
+import Contact from './components/Contact';
+import About from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path="/" render={props => <Home /> } />
+          <Route exact path="/about" render={props => <About /> } />
+          <Route exact path="/projects" render={props => <Projects /> } />
+          <Route exact path="/resume" render={props => <Resume /> } />
+          <Route exact path="/contact" render={props => <Contact /> } />
         </Switch>
       </Router>    
     </>
